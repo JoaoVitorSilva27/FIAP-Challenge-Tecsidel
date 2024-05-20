107e9e03fe3df2c8fc4b97428f5e6359fd9d6206
João Vitor S. <jvsg27@hotmail.com>
2024-05-20 10:38:50 -0300

    Alertas

--- trabalho/src/script.js
+++ trabalho/src/script.js
@@ -145,3 +145,43 @@ const listAlertas = [
     gravidade: "Grave",
   },
 ];
+
+function alerta() {
+  var alertMenu = document.getElementById("alertMenu");
+  var alertListContainer = document.getElementById("alertList");
+  var alertArrow = document.querySelector(".alert-arrow"); 
+
+  alertListContainer.innerHTML = '';
+
+  var alerta = listAlertas[1];
+  var alertItem = document.createElement("div");
+  alertItem.classList.add("alert-item");
+  alertItem.innerHTML = "<strong>Placa:</strong> " + alerta.placa + ", <strong>Infracao:</strong> " + alerta.infracao + ", <strong>Gravidade:</strong> " + alerta.gravidade;
+  alertListContainer.appendChild(alertItem);
+
+  if (alertMenu.style.display === "block") {
+    alertMenu.style.display = "none";
+    alertArrow.style.display = "none";
+  } else {
+    alertMenu.style.display = "block";
+    alertArrow.style.display = "block";
+  }
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.fa-bell')) {
+    var alertMenus = document.getElementsByClassName("alert-menu");
+    for (var i = 0; i < alertMenus.length; i++) {
+      var openAlertMenu = alertMenus[i];
+      if (openAlertMenu.style.display === "block") {
+        openAlertMenu.style.display = "none";
+
+        var alertArrow = document.querySelector(".alert-arrow");
+        if (alertArrow.style.display === "block") {
+          alertArrow.style.display = "none";
+        }
+      }
+    }
+  }
+}
+
